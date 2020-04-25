007bbe81933c625dc7777b19908cc7059d147fac
nino <nino_roldan@yahoo.com>
2020-04-25 22:39:00 +0900

    used partials

--- btre/settings.py
+++ btre/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #when collecstatic is done, django looks for static folders in all apps and moves them here.
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
