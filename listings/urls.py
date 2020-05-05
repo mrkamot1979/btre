b0d61112eab308b77229a6a9c55a0e485d0c15e1
nino <nino_roldan@yahoo.com>
2020-05-05 23:35:00 +0900

    fixed Listings URL

--- listings/urls.py
+++ listings/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='listings'), #this one goes to /listings
-    path('<int: listing_id>', views.listing, name='listing'), #example url would bre /listing/id where the id will be the listing_id parameter
+    path('<int:listing_id>', views.listing, name='listing'), #example url would bre /listing/id where the id will be the listing_id parameter
     path('search', views.search, name='search') #this is for searching
 ]
