67ec460bd80f5679a10463514af3a0c4565843e8
nino <nino_roldan@yahoo.com>
2020-05-09 21:23:48 +0900

    fixed the listings comment

--- listings/urls.py
+++ listings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='listings'), #this one goes to /listings
+    path('', views.index, name='listings'), #handles the url /listings
     path('<int:listing_id>', views.listing, name='listing'), #example url would bre /listing/id where the id will be the listing_id parameter
     path('search', views.search, name='search') #this is for searching, url is /listings/search
 ]
