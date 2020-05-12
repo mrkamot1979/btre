b765bec4ffd22d0f4d60e5d18fb18faa72c33229
nino <nino_roldan@yahoo.com>
2020-05-12 16:00:55 +0900

    setting up the databases

--- btre/settings.py
+++ btre/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Kawasaki2601',
+        'HOST': 'localhost'
     }
 }
 
