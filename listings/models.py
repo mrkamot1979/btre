b622ddecedcb868b596c95be6d90e4361e0682e3
nino <nino_roldan@yahoo.com>
2020-05-17 17:20:29 +0900

    Fixed the blank paramters

--- listings/models.py
+++ listings/models.py
@@ -18,12 +18,12 @@ class Listing(models.Model):
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to = 'photos/%Y/%m/%d/') #anything that we upload from the admin will go to the media folder
-    photo_1 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blanke=True)
-    photo_2 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blanke=True)
-    photo_3 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blanke=True)
-    photo_4 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blanke=True)
-    photo_5 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blanke=True)
-    photo_6 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blanke=True)
+    photo_1 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     lst_date = models.DateTimeField(default=datetime.now, blank=True) #this is imported from the module datetime
     def __str_(self): #this method is to specify which main field we want to display
