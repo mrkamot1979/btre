b6d20bed31424b449304b99d9d21b5ec371f73ed
nino <nino_roldan@yahoo.com>
2020-05-19 21:38:19 +0900

    Added Listings and Realtors in Admin site

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+#this page essentially handles he configuration for the admin pages for listings e.g. add, delete, edit Listings via the django administration
+from .models import Listing #bring from listings > models
 
-# Register your models here.
+admin.site.register(Listing) #this would enable the Listing entry in the Admin site
