01e37e3f0a3b49f60f633a92bb7df351e3dd48e2
nino <nino_roldan@yahoo.com>
2020-06-05 13:12:47 +0900

    Listings field in Admin

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 #this page essentially handles he configuration for the admin pages for listings e.g. add, delete, edit Listings via the django administration
 from .models import Listing #bring from listings > models
 
-admin.site.register(Listing) #this would enable the Listing entry in the Admin site
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published')
+
+admin.site.register(Listing, ListingAdmin) #this would enable the Listing entry in the Admin site
