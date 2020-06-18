b21cbd40e058328e623f50a77183174c22e12809
nino <nino_roldan@yahoo.com>
2020-06-18 16:45:31 +0900

    Updated pip to 2.0.1

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from .models import Listing
+from .models import Listing #bringin in the Listing model
+
+
 
 def index(request):
     return render(request, 'listings/listings.html')
+
+    listings = Listing.objects.all() 
 
 def listing(request):
     return render(request, 'listings/listing.html')
